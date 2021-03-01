b5787d74e045729a99f8ac30ed6921ba471c3cea
kimhongju <kinhoz9321@gmail.com>
2021-03-01 23:58:24 +0900

    dao service delete detail 추가

---
+++ SpringProject_EBD/src/main/java/com/acorn/ebd/report/service/ReportService.java
@@ -0,0 +1,24 @@
+package com.acorn.ebd.report.service;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import com.acorn.ebd.report.dto.ReportDto;
+
+public interface ReportService {
+	
+	public void replaceInfo(String booktitle, String author, String link, ModelAndView mView);
+	
+	//새 독후감을 저장하는 메소드
+	public void saveContent(ReportDto dto, HttpServletRequest request);
+	
+	//독후감을 불러오는 메소드
+	public void getList(ModelAndView mView, HttpServletRequest request);
+	
+	//독후감 하나의 정보를 불러오는 메소드
+	public void getDetail(int num, ModelAndView mView);
+	
+	//독후감을 삭제하는 메소드
+	public void deleteContent(int num);
+}
