5499830821f76d28ff9eaa5790eb9e53b915bb78
dasom <dasom@DESKTOP-JBCQKQI>
2021-03-23 13:04:54 +0900

    파일/마켓 리스트에 보여질 갯수 수정

--- SpringProject_EBD/src/main/java/com/acorn/ebd/file/service/FileServiceImpl.java
+++ SpringProject_EBD/src/main/java/com/acorn/ebd/file/service/FileServiceImpl.java
@@ -29,7 +29,7 @@ public class FileServiceImpl implements FileService{
 	@Override
 	public void getList(HttpServletRequest request) {
 		//한 페이지에 나타낼 row 의 갯수
-		final int PAGE_ROW_COUNT=5;
+		final int PAGE_ROW_COUNT=9;
 		//하단 디스플레이 페이지 갯수
 		final int PAGE_DISPLAY_COUNT=5;
 		//보여줄 페이지의 번호
